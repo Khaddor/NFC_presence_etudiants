762b75042a50f098258cfe81fad17df5eea8a3ad
Mehdi Khaddor <el-mehdi.khaddor@etu.univ-lehavre.fr>
2024-02-21 15:34:58 +0100

    Added the Add a new Student page

---
+++ lib/add_student_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'sheets_service.dart';
+
+class AddStudentPage extends StatefulWidget {
+  final String nfcCode;
+
+  const AddStudentPage({Key? key, required this.nfcCode}) : super(key: key);
+
+  @override
+  _AddStudentPageState createState() => _AddStudentPageState();
+}
